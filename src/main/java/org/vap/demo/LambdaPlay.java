71a09aed0b9ddc6481051263cf281a23617b6ea1
Vahe Pezeshkian <vaheh.pezeshkian@gmail.com>
2018-01-11 22:45:10 +0400

    Using json inputs with typed Java classes

--- src/main/java/org/vap/demo/LambdaPlay.java
+++ src/main/java/org/vap/demo/LambdaPlay.java
@@ -3,9 +3,26 @@ package org.vap.demo;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 
-public class LambdaPlay implements RequestHandler<String, String> {
-    public String handleRequest(String input, Context context) {
-        context.getLogger().log("Input: " + input);
-        return input.toUpperCase();
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class LambdaPlay implements RequestHandler<List<Person>, String> {
+
+    private static final Person DEFAULT = new Person();
+    static {
+        DEFAULT.setName("Default Person");
+        DEFAULT.setAge(0);
+    }
+
+    public String handleRequest(List<Person> input, Context context) {
+
+        Person oldest = input.stream().max(Comparator.comparingInt(Person::getAge)).orElse(DEFAULT);
+        Person youngest = input.stream().min(Comparator.comparingInt(Person::getAge)).orElse(DEFAULT);
+
+        StringJoiner response = new StringJoiner(", ");
+        response.add("Youngest: " + youngest.getName() + " (" + youngest.getAge() + ")");
+        response.add("Oldest: " + oldest.getName() + " (" + oldest.getAge() + ")");
+        return response.toString();
     }
 }
